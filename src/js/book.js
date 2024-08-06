d3987333059325a980087a2812cd7042c352defc
Jeffrey Keem <keemaje5@gmail.com>
2024-08-05 20:21:57 -0700

    #1 added reading and want to read states in book class and removed unnecessary display function

--- src/js/book.js
+++ src/js/book.js
@@ -1,17 +1,16 @@
+const readStatus = {
+    READ: 'read',
+    READING: 'reading',
+    WANT_TO_READ: 'want-to-read',
+    UNREAD: 'unread'
+};
+
 export default class Book {
-    title = 'Unknown';
-    author = 'Unknown';
-    pages = 0;
-    read = false;
 
     constructor(title, author, pages, read) {
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.read = read;
-    }
-
-    displayInfo() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, Read: ${this.read}`;
     }
 }
