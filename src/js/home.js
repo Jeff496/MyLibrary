c2eeb3b00b23a876e3d8048c2f37025a8a4a8cec
Jeffrey Keem <keemaje5@gmail.com>
2024-08-05 18:45:34 -0700

    styling and format changes

--- src/js/home.js
+++ src/js/home.js
@@ -1,20 +1,11 @@
-import { userLibrary } from "../index.js";
-
 export default function makeHome() {
     const home = document.createElement('div');
 
     const headline = document.createElement('h1');
     headline.textContent = 'Welcome the Library';
     headline.classList.add('headline');
-    home.appendChild(headline);
-
-    const form = document.createElement('form');
-    home.appendChild(form);
-
-    const addBookButton = document.createElement('button');
-    addBookButton.textContent = 'Add New Book';
-    home.append(addBookButton);
     
-
+    home.appendChild(headline);
+    
     return home;
 }
