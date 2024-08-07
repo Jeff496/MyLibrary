a85c9a3ecb562a1277941b522e710d97dadb20fa
Jeffrey Keem <keemaje5@gmail.com>
2024-08-07 02:42:12 -0700

    functionality and styling for carousel #6

--- src/js/home.js
+++ src/js/home.js
@@ -24,9 +24,9 @@ function imageCarousel() {
   const leftArrowButton = document.createElement("button");
   leftArrowButton.classList.add("leftArrowButton");
   leftArrowButton.innerHTML = `<img src="${leftArrow}" alt="Left Arrow Button">`;
-  const rightArrowbutton = document.createElement("button");
-  rightArrowbutton.classList.add("rightArrowButton");
-  rightArrowbutton.innerHTML = `<img src="${rightArrow}" alt="Left Arrow Button">`;
+  const rightArrowButton = document.createElement("button");
+  rightArrowButton.classList.add("rightArrowButton");
+  rightArrowButton.innerHTML = `<img src="${rightArrow}" alt="Left Arrow Button">`;
 
   // main display area
   const container = document.createElement("div");
@@ -35,27 +35,97 @@ function imageCarousel() {
   carouselTrack.classList.add("carouselTrack");
   container.appendChild(carouselTrack);
 
+  const allBooks = []; // array that stores all bookcards
+  let currentBookIndex = 0;
+
   featuredBooks.forEach((book) => {
     const carouselItem = document.createElement("li");
     carouselItem.classList.add("carouselItem");
     carouselItem.appendChild(makeBookCards(book, buttonOption.ADD));
     carouselTrack.appendChild(carouselItem);
+    allBooks.push(carouselItem);
   });
 
   // current slide indicator dots
   const indicators = document.createElement("div");
   indicators.classList.add("indicators");
 
-  for (let i = 0; i < featuredBooks.length; i++) {
+  featuredBooks.forEach((_, index) => {
     const indicatorButton = document.createElement("button");
     indicatorButton.classList.add("indicatorButton");
+    indicatorButton.dataset.index = index;
     indicators.appendChild(indicatorButton);
+  });
+
+  const indicatorButtons = indicators.querySelectorAll(".indicatorButton");
+  indicatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const index = parseInt(button.dataset.index);
+      showBook(index, allBooks, button);
+      currentBookIndex = index;
+    });
+  });
+
+  showBook(0, allBooks, indicatorButtons[0]);
+
+  // arrow functionality
+
+  leftArrowButton.addEventListener("click", () => {
+    currentBookIndex--;
+    console.log(currentBookIndex);
+    if (currentBookIndex < 0) {
+      currentBookIndex = allBooks.length - 1;
+    }
+    showBook(currentBookIndex, allBooks, indicatorButtons[currentBookIndex]);
+  });
+
+  rightArrowButton.addEventListener("click", () => {
+    currentBookIndex++;
+    console.log(currentBookIndex);
+    if (currentBookIndex > allBooks.length - 1) {
+      currentBookIndex = 0;
+    }
+    showBook(currentBookIndex, allBooks, indicatorButtons[currentBookIndex]);
+  });
+
+  let autoScroll = setInterval(scroll, 5000);
+
+  function scroll() {
+    currentBookIndex++;
+    if (currentBookIndex > allBooks.length - 1) {
+      currentBookIndex = 0;
+    }
+    showBook(currentBookIndex, allBooks, indicatorButtons[currentBookIndex]);
+  }
+
+  function resetInterval() {
+    clearInterval(autoScroll);
+    autoScroll = setInterval(scroll, 5000);
   }
 
   carousel.appendChild(leftArrowButton);
   carousel.appendChild(container);
-  carousel.appendChild(rightArrowbutton);
+  carousel.appendChild(rightArrowButton);
   carousel.appendChild(indicators);
 
   return carousel;
+}
+
+function showBook(index, allBooks, button) {
+  const bookCount = allBooks.length;
+  if (index > -1 && index < bookCount) {
+    const indicatorButtons = document.querySelectorAll(".indicatorButton");
+    indicatorButtons.forEach((button) => {
+      button.classList.remove("currentBook");
+    });
+
+    allBooks.forEach((book, i) => {
+      if (index === i) {
+        book.style.transform = "translateX(-200%)";
+        button.classList.add("currentBook");
+      } else {
+        book.style.transform = "translateX(0%)";
+      }
+    });
+  }
 }
